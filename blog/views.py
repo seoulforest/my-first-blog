1602aaa03fdbb93faa47b1ea600a73c82e4861e6
seoulforest <chlwnsdyd89@gmail.com>
2019-05-18 17:23:04 +0900

    mod bootstrap css button location

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     ##posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
